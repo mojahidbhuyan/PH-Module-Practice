6f2b0d5aed9bee635a9b83ebf2a6fc439f35dba9
Mojahid Bhuyan <54664829+mojahidbhuyan@users.noreply.github.com>
2025-08-08 00:50:50 +0300

    file added

---
+++ Module-18/traverse_array.js
@@ -0,0 +1,23 @@
+// For Loop Example: 
+
+const fruits = [ 'Apple', 'Banana', 'Anar', 'Watermalone' , 'Daav']
+
+for (let i = 0; i< fruits.length; i++){
+    // console.log(i);
+    
+     console.log(fruits[i]);
+}
+
+
+// -------------------------------------- 
+
+// While Loop Example: 
+
+const wfruits = [ 'Komla', 'Banana', 'Anar', 'Watermalone' , 'Licho']
+
+let k = 0 ;
+while (k< wfruits.length) {
+    console.log(wfruits[k])
+    k++;
+}
+
